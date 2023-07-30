d1f5d0f03b1571b96ff45331409e3e98a15417cd
Freddie Hurson <fredhurs@gmail.com>
2023-07-30 18:16:35 -0400

    Initial Commit

---
+++ Shared/Gym_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_TrackerApp.swift
+//  Shared
+//
+//  Created by Freddie Hurson on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Gym_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
